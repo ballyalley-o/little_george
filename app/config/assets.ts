ac5f05c011f4f5bbf19b23a84fb12a0fa6b9386b
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-14 12:17:13 +1300

    initialize constant ts files added ready'd exports

---
+++ app/config/assets.ts
@@ -0,0 +1,10 @@
+const ASSET = {
+  logo: {
+    src: '/assets/brand/little-george.svg',
+    width: 27,
+    height: 27,
+    alt: 'brand-logo',
+  },
+}
+
+export default ASSET
--- app/config/index.ts
+++ app/config/index.ts
@@ -1,1 +1,2 @@
 export { default as metadata } from '@config/meta'
+export { default as ASSETS } from '@config/assets'
