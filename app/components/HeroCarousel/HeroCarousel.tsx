5a604464b32a392b46b77f8c9d4b56df2450fae7
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-14 20:28:49 +1300

    hero carousel

--- app/components/HeroCarousel/HeroCarousel.tsx
+++ app/components/HeroCarousel/HeroCarousel.tsx
@@ -1,9 +1,26 @@
 'use client'
-
-import React from 'react'
+// @css
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+// @components
+import { Carousel } from 'react-responsive-carousel'
+import Image from 'next/image'
+// @assets & @configurations
+import { ASSET, SETTING } from '@config'
 
 const HeroCarousel = () => {
-  return <div>HeroCarousel</div>
+  return (
+    <Carousel {...SETTING.carousel_home}>
+      {ASSET.mock_products.map((image, index) => (
+        <Image
+          key={index}
+          src={image.imgUrl}
+          alt={image.alt}
+          className='object-contain'
+          {...ASSET.sizes.images}
+        />
+      ))}
+    </Carousel>
+  )
 }
 
 export default HeroCarousel
