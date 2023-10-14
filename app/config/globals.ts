00ee38f0c8899e3c7a83813f395af05ffaabead1
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-15 04:56:56 +1300

    globals for bright data

--- app/config/globals.ts
+++ app/config/globals.ts
@@ -2,6 +2,11 @@ import { ASSET } from '@config'
 
 const GLOBAL = {
   api: '',
+  bd_user: process.env.BRIGHT_DATA_USER,
+  bd_pass: process.env.BRIGHT_DATA_PASS,
+  bd_port: process.env.BRIGHT_DATA_PORT,
+  bd_host: process.env.BRIGHT_DATA_HOST,
+  bd_base_curl: process.env.BASE_CURL,
   brand_icon: ASSET.nav,
   nav_icon_size: ASSET.sizes.navIcon,
 }
