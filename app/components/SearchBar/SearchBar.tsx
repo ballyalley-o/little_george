7178125242b3ad0d5ae384f0da5b980bd546e0d8
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-15 02:31:32 +1300

    spinner component, init in searchbar

--- app/components/SearchBar/SearchBar.tsx
+++ app/components/SearchBar/SearchBar.tsx
@@ -1,18 +1,49 @@
 'use client'
+
+import { FormEvent, useState } from 'react'
+// @components
+import { Spinner } from '@components'
 // @constants
 import { TITLE } from '@constants'
+// @assets
+import { toast } from 'react-toastify'
+// @utils
+import { isAmazonProductValid } from '@utils'
 
 const SearchBar = () => {
-  const handleSubmit = () => {}
+  const [searchPrompt, setSearchPrompt] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const isLinkValid = isAmazonProductValid(searchPrompt)
+
+    if (!isLinkValid) return toast.error('Provide a Valid Link')
+
+    try {
+      setIsLoading(true)
+    } catch (error: any) {
+      toast.error(error.message)
+    } finally {
+      setIsLoading(false)
+    }
+  }
   return (
     <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
       <input
         type='text'
+        value={searchPrompt}
         placeholder='Enter Product'
         className='searchbar-input'
+        onChange={(e: any) => setSearchPrompt(e.target.value)}
       />
-      <button type='submit' className='searchbar-btn'>
-        {TITLE.search}
+      <button
+        type='submit'
+        className='searchbar-btn'
+        disabled={searchPrompt === ''}
+      >
+        {isLoading ? <Spinner /> : TITLE.search}
       </button>
     </form>
   )
--- app/components/index.tsx
+++ app/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as HeroCarousel } from '@components/HeroCarousel/HeroCarousel'
 export { default as Navbar } from '@components/Navbar/Navbar'
 export { default as SearchBar } from '@components/SearchBar/SearchBar'
+export { default as Spinner } from '@components/Spinner/Spinner'
