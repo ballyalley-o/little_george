5e48f7fbb764226eb3a8d45fcf1a3bffc1d0cfa7
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-15 02:32:18 +1300

    lang config update

---
+++ app/config/lang.ts
@@ -0,0 +1,16 @@
+const LANG = {
+  ar: 'ar',
+  de: 'de',
+  en: 'en',
+  es: 'es',
+  fr: 'fr',
+  it: 'it',
+  ja: 'ja',
+  ko: 'ko',
+  ptbr: 'pt-br',
+  ru: 'ru',
+  zhcn: 'zh-cn',
+  zhtw: 'zh-tw',
+}
+
+export default LANG
