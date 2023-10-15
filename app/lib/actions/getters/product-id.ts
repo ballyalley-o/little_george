d211e4f42914e3177af0adab3bc3e712e49ee682
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-16 02:09:22 +1300

    getter product by id

---
+++ app/lib/actions/getters/product-id.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import { connectDb } from '@lib/db'
+// @actions
+import { scrapeProduct } from '@lib/scraper'
+import {
+  getAveragePrice,
+  getHighestPrice,
+  getLowestPrice,
+} from '@utils/getters'
+// @utils
+import { logger } from '@utils'
+import { Product } from '@lib/models'
+
+async function getProductById(prodId: string) {
+  try {
+    connectDb()
+    const product = await Product.findOne({ _id: prodId })
+
+    if (!product) return null
+
+    return product
+  } catch (error: any) {
+    logger.error(error)
+  }
+}
