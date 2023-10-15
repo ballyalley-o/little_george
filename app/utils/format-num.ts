aa94fe6bfa5856ae57a601645a37e3ab69250cf1
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-15 19:04:55 +1300

    format num utils

---
+++ app/utils/format-num.ts
@@ -0,0 +1,8 @@
+const formatNum = (num: number = 0) => {
+  return num.toLocaleString(undefined, {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })
+}
+
+export default formatNum
--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,5 +1,7 @@
 export { default as isAmazonProductValid } from '@utils/validate-link'
 export { default as logger } from '@utils/logger'
+export { default as formatNum } from '@utils/format-num'
 export { default as extractPrice } from '@utils/extract-price'
 export { default as extractCurrency } from '@utils/extract-currency'
 export { default as extractDiscountRate } from '@utils/extract-discount-rate'
+export { default as extractDescription } from '@utils/extract-description'
