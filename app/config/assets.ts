1c45fd136a8bb9a37777b6b308997748d1402a49
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-16 23:48:56 +1300

    icons assets globals

--- app/config/assets.ts
+++ app/config/assets.ts
@@ -1,13 +1,43 @@
 const ASSET = {
   sizes: {
+    _def_icon: {
+      width: 20,
+      height: 20,
+    },
     navIcon: {
       width: 28,
       height: 28,
+    },
+    product_image: {
+      width: 200,
+      height: 200,
+    },
+    product_detail: {
+      width: 580,
+      height: 400,
     },
     images: {
       width: 484,
       height: 484,
     },
+  },
+  heart_icon: {
+    src: '/assets/icons/red-heart.svg',
+    alt: 'heart',
+    width: 20,
+    height: 20,
+  },
+  bookmark_icon: {
+    src: '/assets/icons/bookmark.svg',
+    alt: 'bookmark',
+    height: 20,
+    width: 20,
+  },
+  share_icon: {
+    src: '/assets/icons/share.svg',
+    alt: 'share',
+    height: 20,
+    width: 20,
   },
   logo: {
     src: '/assets/brand/little-george.svg',
