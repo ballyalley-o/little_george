36353651425513ee17fad2535a38f3af451bf07f
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-18 03:23:44 +1300

    added description to model

--- app/lib/models/product.ts
+++ app/lib/models/product.ts
@@ -15,6 +15,10 @@ const productSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    description: {
+      type: String,
+      required: true,
+    },
     image: {
       type: String,
       required: true,
