18f2b97549cf9518d4eb2fb207d08cec90945136
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-22 14:25:29 +1300

    add email to prod func and export

---
+++ app/lib/actions/email-product.ts
@@ -0,0 +1,35 @@
+'use server'
+
+import { logger } from '@utils'
+// @model
+import { Product } from '@lib/models'
+import { User } from '@types'
+// @mailer
+import { generateEmailBody } from '@lib/nodemailer'
+import sendEmail from '@lib/nodemailer/send-email'
+
+async function addEmailToProduct(productId: string, userEmail: string) {
+  try {
+    const product = await Product.findById(productId)
+
+    if (!product) return
+
+    const userExists = product.users.some(
+      (user: User) => user.email === userEmail
+    )
+
+    if (!userExists) {
+      product.users.push({ email: userEmail })
+
+      await product.save()
+
+      const emailContent = await generateEmailBody(product, 'WELCOME')
+
+      await sendEmail(emailContent, [userEmail])
+    }
+  } catch (error: any) {
+    logger.error(error)
+  }
+}
+
+export default addEmailToProduct
--- app/lib/actions/index.ts
+++ app/lib/actions/index.ts
@@ -1,2 +1,3 @@
 export { default as scrapeAndStoreProduct } from '@lib/actions/scrape-store'
 export { default as relatedProducts } from '@lib/actions/product-related-length'
+export { default as addEmailToProduct } from '@lib/actions/email-product'
