a2c22105ecbb3b15aa4e253fb885ecd541387b97
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-23 13:25:59 +1300

    email notif changes

--- app/utils/getters/email-notification.ts
+++ app/utils/getters/email-notification.ts
@@ -1,12 +1,7 @@
 import { getLowestPrice } from '@utils/getters'
 import { Product } from '@types'
-
-const Notification = {
-  WELCOME: 'WELCOME',
-  CHANGE_OF_STOCK: 'CHANGE_OF_STOCK',
-  LOWEST_PRICE: 'LOWEST_PRICE',
-  THRESHOLD_MET: 'THRESHOLD_MET',
-}
+// @constants
+import { NOTIFICATION } from '@constants'
 
 const THRESHOLD_PERCENTAGE = 40
 
@@ -14,15 +9,15 @@ function getEmailNotification(scrapedProd: Product, currProd: Product) {
   const lowestPrice = getLowestPrice(currProd.priceHistory)
 
   if (scrapedProd.currentPrice < lowestPrice) {
-    return Notification.LOWEST_PRICE as keyof typeof Notification
+    return NOTIFICATION.LOWEST_PRICE as keyof typeof NOTIFICATION
   }
 
   if (!scrapedProd.isOutOfStock && currProd.isOutOfStock) {
-    return Notification.CHANGE_OF_STOCK as keyof typeof Notification
+    return NOTIFICATION.CHANGE_OF_STOCK as keyof typeof NOTIFICATION
   }
 
   if (scrapedProd.discountRate > THRESHOLD_PERCENTAGE) {
-    return Notification.THRESHOLD_MET as keyof typeof Notification
+    return NOTIFICATION.THRESHOLD_MET as keyof typeof NOTIFICATION
   }
   return null
 }
