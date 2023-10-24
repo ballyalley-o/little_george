7109324f7a15b9f62a96579c1a01fb8b946d7083
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-25 00:29:05 +1300

    form input refax

--- app/components/Form/FormInput.tsx
+++ app/components/Form/FormInput.tsx
@@ -4,31 +4,51 @@
 import Image from 'next/image'
 // @globals
 import { ASSET } from '@config'
-// @constants
-import { CONTENT } from '@constants'
 // @styles
 import * as _ from '@assets/styles'
 // @interface
 import { FormInput } from '@interfaces/components'
 
-const Input = ({ icon, type, label, value, setChange }: FormInput) => {
+const Input = ({
+  icon,
+  type,
+  label,
+  value,
+  setChange,
+  searchBar,
+  className,
+}: FormInput) => {
   return (
     <>
-      <label htmlFor={type} className={_.modal.StyledLabel}>
-        {label}
-      </label>
-      <div className={_.form.StyledFormContainerDiv}>
-        <Image {...ASSET.form(icon)} />
+      {!searchBar && (
+        <label htmlFor={type} className={_.modal.StyledLabel}>
+          {label}
+        </label>
+      )}
+      {!searchBar ? (
+        <div className={_.form.StyledFormContainerDiv}>
+          <Image {...ASSET.form(icon)} />
+          <input
+            required
+            type={type}
+            id={type}
+            placeholder={label}
+            className={className}
+            value={value}
+            onChange={(e) => setChange(e.target.value)}
+          />
+        </div>
+      ) : (
         <input
           required
           type={type}
           id={type}
           placeholder={label}
-          className={_.form.StyledFormInput}
+          className={className}
           value={value}
           onChange={(e) => setChange(e.target.value)}
         />
-      </div>
+      )}
     </>
   )
 }
