185d6b051081a8a3040e2fb7424bc362bf0934b2
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-25 00:28:42 +1300

    product card refax

--- app/components/Product/ProductCard.tsx
+++ app/components/Product/ProductCard.tsx
@@ -2,29 +2,31 @@ import React from 'react'
 // @components
 import Link from 'next/link'
 import Image from 'next/image'
+// @styles
+import * as _ from '@assets/styles/product'
 // @interfaces
 import { ProductProps } from '@interfaces/components'
 // @assets
 import { ASSET } from '@config'
+// @constants
+import { PATH } from '@constants'
 
 const ProductCard = ({ product }: ProductProps) => {
   return (
-    <Link href={`/products/${product._id}`} className='product-card'>
-      <div className='product-card_img-container'>
+    <Link href={PATH.product(product)} className={_.StyledWrapperLink}>
+      <div className={_.StyledWrapperDiv}>
         <Image
           src={product.image}
           alt={product.title}
           {...ASSET.sizes.product_image}
-          className='product-card_img'
+          className={_.StyledProductImage}
         />
       </div>
-      <div className='flex flex-col gap-3'>
-        <h3 className='product-title'>{product.title}</h3>
-        <div className='flex justify-between'>
-          <p className='text-black opacity-50 text-lg capitalize'>
-            {product.category}
-          </p>
-          <p className='text-black text-lg font-semibold'>
+      <div className={_.StyledProductDiv}>
+        <h3 className={_.StyledProductTitleH3}>{product.title}</h3>
+        <div className={_.StyledContentDiv}>
+          <p className={_.StyledCategoryP}>{product.category}</p>
+          <p className={_.StyledProductPriceP}>
             <span>{product?.currency}</span>
             <span>{product?.currentPrice}</span>
           </p>
