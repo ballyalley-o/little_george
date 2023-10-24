58a3101661f9e5c4c2b68a2cc53d32d51b40c40a
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-24 23:20:32 +1300

    Price Info component refax

--- app/components/Product/PriceInfoCard.tsx
+++ app/components/Product/PriceInfoCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 // @components
 import Image from 'next/image'
+// @styles
+import * as _ from '@assets/styles/product'
 // @interfaces
 import { PriceInfo } from '@interfaces/components'
 // @assets
@@ -8,11 +10,11 @@ import { ASSET } from '@config'
 
 const PriceInfoCard = ({ title, icon, value, borderColor }: PriceInfo) => {
   return (
-    <div className={`price-info_card border-lp-[${borderColor}]`}>
-      <p className='text-base text-black0199'>{title}</p>
-      <div className='flex gap-1'>
+    <div className={_.StyledContainerDiv(borderColor)}>
+      <p className={_.StyledTitleP}>{title}</p>
+      <div className={_.StyledLogoWrapperDiv}>
         <Image src={icon} alt={title} {...ASSET.sizes.price_info} />
-        <p className='text-2xl font-bold text-secondary'>{value}</p>
+        <p className={_.StyledValueP}>{value}</p>
       </div>
     </div>
   )
