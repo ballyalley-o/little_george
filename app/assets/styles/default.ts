b63007185c73aa47c0a6809324b0f79ea294a07a
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-06 01:51:07 +1300

    refactored styles for home page

--- app/assets/styles/default.ts
+++ app/assets/styles/default.ts
@@ -1,3 +1,12 @@
 import classNames from 'classnames'
 
 export const StyledBtn = classNames('btn')
+export const StyledContainerSection = classNames('px-6 md:px-20 py-24')
+export const StyledWrapperDiv = classNames('flex max-xl:flex-col gap-16')
+export const StyledHeroWrapperDiv = classNames('flex flex-col justify-center')
+export const StyledHeroTitleP = classNames('small-text')
+export const StyledHeroBrandH1 = classNames('head-text')
+export const StyledHeroDescP = classNames('mt-6')
+export const StyledTrendingWrapper = classNames('trending-section')
+export const StyledSectionTextH2 = classNames('section-text')
+export const StyledMapContainer = classNames('flex flex-wrap gap-x-8 gap-y-16')
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import { getProducts } from '@lib/actions/getters'
 // @components
 import Image from 'next/image'
 import { SearchBar, HeroCarousel, ProductCard } from '@components'
+// @styles
+import { def } from '@assets/styles'
 // @constants
 import { BRAND, TITLE } from '@constants'
 
@@ -13,23 +15,23 @@ const Home = async () => {
   const products = await getProducts()
   return (
     <>
-      <section className='px-6 md:px-20 py-24 '>
-        <div className='flex max-xl:flex-col gap-16'>
-          <div className='flex flex-col justify-center'>
-            <p className='small-text'>
+      <section className={def.StyledContainerSection}>
+        <div className={def.StyledWrapperDiv}>
+          <div className={def.StyledHeroWrapperDiv}>
+            <p className={def.StyledHeroTitleP}>
               See All the Prices Here:
               <Image {...ASSET.arrow_r} />
             </p>
-            <h1 className='head-text'>{BRAND.hero}</h1>
-            <p className='mt-6'>{BRAND.desc}</p>
+            <h1 className={def.StyledHeroBrandH1}>{BRAND.hero}</h1>
+            <p className={def.StyledHeroDescP}>{BRAND.desc}</p>
             <SearchBar />
           </div>
           <HeroCarousel />
         </div>
       </section>
-      <section className='trending-section'>
-        <h2 className='section-text'>{TITLE.trending}</h2>
-        <div className='flex flex-wrap gap-x-8 gap-y-16'>
+      <section className={def.StyledTrendingWrapper}>
+        <h2 className={def.StyledSectionTextH2}>{TITLE.trending}</h2>
+        <div className={def.StyledMapContainer}>
           {products?.map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
